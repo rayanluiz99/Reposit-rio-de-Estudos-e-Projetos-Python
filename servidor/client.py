cb789b633af90976c20e6a153b1972de43d7ef9c
rayanluiz99 <rayansouza.rl@gmail.com>
2025-07-28 15:52:14 -0300

    teste ssh

--- servidor/client.py
+++ servidor/client.py
@@ -1,14 +1,61 @@
 import socket
+import threading
+from tkinter import *
+import tkinter
+from tkinter import simpledialog
 
-HOST = 'localhost'
-PORT = 8000
+class Chat:
+    def __init__(self):
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        HOST = 'localhost'
+        PORT = 55554
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((HOST, PORT))
+        login = Tk()
+        login.withdraw()
 
-sock.connect((HOST, PORT))
+        self.janela_carregada = False
+        self.ativo = True
 
-sock.send('enviado'.encode())
+        self.nome = simpledialog.askstring('Nome', 'Digite seu nome!', parent=login)
+        self.sala = simpledialog.askstring('Sala', 'Digite a sala que deseja entrar!', parent=login)
+        thread = threading.Thread(target=self.conecta)
+        thread.start()
+        self.janela()
+    def janela(self):
+        self.root = Tk()
+        self.root. geometry("800x800")
+        self.root.title('Chat')
 
-confirmacao = sock.recv(1024).decode()
-if confirmacao == 'ok':
-    print('mensagem recebida')
+        self.caixa_texto = Text(self.root)
+        self.caixa_texto.place(relx=0.05, rely= 0.01, width=700, height= 600 )
+
+        self.envia_mensagem = Entry(self.root)
+        self.envia_mensagem.place(relx=0.05, rely= 0.8, width=500, height= 20 )
+
+        self.btn_enviar = Button(self.root, text='Enviar', command=self.EnviarMensagem)
+        self.btn_enviar.place(relx=0.7, rely= 0.8, width=100, height= 20)
+        self.root.protocol('WM_DELETE_WINDOW', self.fechar)
+
+        self.root.mainloop()
+        
+    def fechar(self):
+        self.root.destroy()
+        self.client.close()
+
+    def conecta(self):
+        while True:
+            recebido = self.client.recv(1024)
+            if recebido == b'SALA':
+                self.client.send(self.sala.encode())
+                self.client.send(self.nome.encode())
+            else:
+                try:
+                    self.caixa_texto.insert('end', recebido.decode())    
+                except:
+                    pass    
+    def EnviarMensagem(self):
+        mensagem = self.envia_mensagem.get()
+        self.client.send(mensagem.encode())
+    
+chat = Chat()        
--- servidor/server.py
+++ servidor/server.py
@@ -1,16 +1,37 @@
 import socket
+import threading
 
 HOST = 'localhost'
-PORT = 8000
+PORT = 55554
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock.bind((HOST, PORT))
+server.bind((HOST, PORT))
+server.listen()
 
-sock.listen(5)
+salas = {}
+
+def broadcast(sala, mensagem):
+    for i in salas[sala]:
+        if isinstance(mensagem, str):
+            mensagem = mensagem.encode()
+        i.send(mensagem)
+def enviarMensagem(nome, sala, client):
+    while True:
+        mensagem = client.recv(1024) 
+        mensagem = f'{nome}: {mensagem.decode()}\n'   
+        broadcast(sala, mensagem)   
 
 while True:
-    novoSock, _ = sock.accept()
-    mensagem = novoSock.recv(1024).decode()
-    print(mensagem)
-    novoSock.send('ok'.encode()r5)
+    client, addr = server.accept()
+    client.send(b'SALA')
+    sala = client.recv(1024).decode()
+    nome = client.recv(1024).decode()
+
+    if sala not in salas.keys():
+        salas[sala] = []
+    salas[sala].append(client)
+    print(f'{nome} se conectou na sala {sala}! INFO {addr}')
+    broadcast(sala, f'{nome}: entrou na sala!\n')
+    thread = threading.Thread(target=enviarMensagem, args=(nome, sala, client))
+    thread.start()
