30cdb182b97759ed8a66d1c8458a3c2a1e3668a1
rayanluiz99 <rayansouza.rl@gmail.com>
2025-07-30 16:49:23 -0300

    adicionando a classe SessaoAvulsaAnestesia na  model sessao

--- anestesia_vet/models/sessao.py
+++ anestesia_vet/models/sessao.py
@@ -4,7 +4,19 @@ from datetime import datetime
 
 class SessaoAnestesia(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    id_animal: int = Field(foreign_key="animal.id")
+    id_animal: Optional[int] = Field(default=None, foreign_key=True)
     id_farmaco: int = Field(foreign_key="farmaco.id")
     dose_utilizada_ml: float
     observacoes: Optional[str] = None
+
+
+class SessaoAvulsaAnestesia(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    especie: str
+    nome_animal: str
+    peso_kg: float
+    id_farmaco: int
+    dose_utilizada_ml: float
+    observacoes: Optional[str] = None
+    data: datetime = Field(default_factory=datetime.now)
+    
